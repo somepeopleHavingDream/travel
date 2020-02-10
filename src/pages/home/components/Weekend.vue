febeada62c976480f0ee6206d840b90d7be97e0c
yangxin <1393003255@qq.com>
2020-02-10 14:53:13 +0800

    feat: vue项目首页-使用axios发送ajax请求

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -39,7 +39,7 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/mixins.styl'
   .title
-    margin-top: .2rem
+    // margin-top: .2rem
     line-height: .8rem
     background: #eeeeee
     text-indent: .2rem
