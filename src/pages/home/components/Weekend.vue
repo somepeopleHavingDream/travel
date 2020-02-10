574cf98fce08e6a10ba13a370bf54ef452022bb7
yangxin <1393003255@qq.com>
2020-02-10 15:41:54 +0800

    feat: vue项目首页-首页父子组组件间传值

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">周末去哪儿</div>
     <ul>
-      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <div class="item-img-wrapper">
           <img :src="item.imgUrl" alt="" class="item-img">
         </div>
@@ -18,45 +18,41 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-        title: '大连圣亚海洋世界',
-        desc: '浪漫大连首站，浪漫的海洋主题乐园'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-        title: '大连圣亚海洋世界',
-        desc: '浪漫大连首站，浪漫的海洋主题乐园'
-      }]
-    }
+  props: {
+    list: Array
   }
+  // data () {
+  //   return {
+  //     recommendList: [{
+  //       id: '0001',
+  //       imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+  //       title: '大连圣亚海洋世界',
+  //       desc: '浪漫大连首站，浪漫的海洋主题乐园'
+  //     }, {
+  //       id: '0002',
+  //       imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+  //       title: '大连圣亚海洋世界',
+  //       desc: '浪漫大连首站，浪漫的海洋主题乐园'
+  //     }]
+  //   }
+  // }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/mixins.styl'
   .title
-    // margin-top: .2rem
     line-height: .8rem
     background: #eeeeee
     text-indent: .2rem
-  // .item
-  //   overflow: hidden
-  //   display: flex
-  //   height: 1.9rem
   .item-img-wrapper
     overflow: hidden
     height: 0
-    padding-bottom: 33.9%
+    padding-bottom: 37.09%
     .item-img
       width: 100%
   .item-info
-    // flex: 1
     padding: .1rem
-    // min-width: 0
     .item-title
       line-height: .54rem
       font-size: .32rem
@@ -64,11 +60,4 @@ export default {
     .item-desc
       line-height: .4rem
       color: #cccccc
-    // .item-button
-    //   line-height: .44rem
-    //   margin-top: .16rem
-    //   background: #ff9300
-    //   padding: 0 .2rem
-    //   border-radiu: .06rem
-    //   color: #ffffff
 </style>
